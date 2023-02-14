0f718d53cbbc0110c9fccd64af4026d6c6d32e56
Hussein Al Mafrachi <81489485+HusseinAlMafrachi@users.noreply.github.com>
2023-02-14 08:15:14 -0500

    This is a test Java project

---
+++ src/testGitHub.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author hasska
+ */
+public class testGitHub {
+    public static void main (String args[]){
+        
+        System.out.print("Hello world");
+        
+    }
+}
